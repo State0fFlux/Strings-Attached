d961c178c04ce55120899e05880c3602b4bc7e2c
State0fFlux <bmanske505@gmail.com>
2025-03-09 16:18:17 -0700

    Last save b4 changing literally everything
    jk it's not that serious. but working on map design using tiles & extremely simple shapes (will work on graphics in the future)

--- character/mar_v_2.gd
+++ character/mar_v_2.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var immobilized = false
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +12,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor() and not immobilized:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
-	if direction:
+	if direction and not immobilized:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+func immobilize(boolean):
+	immobilized = boolean
